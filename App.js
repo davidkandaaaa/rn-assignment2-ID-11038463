8095fe7c662b0c1c8c91535a80b712a837cd091e
David Paa Kwesi Kanda <149037120+davidkandaaaa@users.noreply.github.com>
2024-05-27 14:33:36 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is David</Text>
+      <Text style={styles.text}>My name is David</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'gray',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  text: {
+    fontSize: 24,
+  }
 });
